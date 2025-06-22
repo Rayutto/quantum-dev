17c52360cbd68bc39adb3d50a3b1b0659ca16885
Rayutto <ryan.jamal@outlook.com>
2025-06-22 20:58:21 +0000

    feat: add python for 1D finitie well model

---
+++ Course/Well_model/1D_finite_well.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from qutip import Qobj, basis, qeye, identity, mesolve
+
+
+# ## Parameters definition
+
+# In[ ]:
+
+
+# Grid settings
+N = 200                  # Number of position points
+L = 1.0                  # Length of the well (in arbitrary units)
+x = np.linspace(0, L, N) # Position grid
+dx = x[1] - x[0]         # Position resolution
+V0 = 10                  # Potential level
+
+
+# ## Operator definition
+
+# In[ ]:
+
+
+# Construct the finite-difference Laplacian (second derivative)
+D2 = -2 * np.eye(N)
+for i in range(N - 1):
+    D2[i, i+1] = D2[i+1, i] = 1
+
+T = -0.5 * Qobj(D2) / dx**2  # Kinetic energy operator
+
+# Initialize potential as zeros
+Vx = np.zeros(N)
+
+# Set potential = V0 outside [1/3, 2/3]
+Vx[x < L/3] = V0
+Vx[x > 2*L/3] = V0
+
+V = Qobj(np.diag(Vx))  # Potential energy operator
+
+H = T + V  # Total Hamiltonian
+
+
+# ## Eigenstate equation resolution
+
+# In[25]:
+
+
+eigenstates = H.eigenstates()
+energies = eigenstates[0]
+states = eigenstates[1]
+
+
+# ## Plot
+
+# In[26]:
+
+
+plt.figure(figsize=(8, 5))
+for i in range(5):
+    psi = states[i].full().flatten()
+    plt.plot(x, psi / np.sqrt(dx), label=f'n={i+1}')
+plt.title("Wavefunctions in Infinite Square Well")
+plt.xlabel("Position x")
+plt.ylabel("Wavefunction ψ(x)")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
